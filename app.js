14827c17375ede9029d339901edfd9fb82b48e85
Tien <dhtien2k2@gmail.com>
2023-09-23 09:10:25 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const contactRouter= require("./app/routes/contact.route");
 
+const ApiError = require("./app/api-error");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/", (req, res) =>{
-    res.json({message: "welcome to contact book application"});
-
+app.use("/api/contacts",contactRouter);
+app.use("/api/contacts",contactRouter);
+app.get("/",(req,res)=> { 
+    res.json({message: "Welcome to contact book application"});
+});
+app.use((req,res,next)=>{
+    return next(new ApiError(404, "Resource not found"));
 });
 
+app.use((err,req,res,next)=>{
+    return res.status(error.statusCode || 500).json({
+        message: error.message|| "Inrenal server Error",
+    });
+});
+
+app.use((err,req,res,next)=>{
+    return res.status(error.statusCode || 500).json({
+        message: error.message|| "Inrenal server Error",
+    });
+});
 module.exports = app;
